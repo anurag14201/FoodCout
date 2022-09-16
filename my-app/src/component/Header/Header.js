cb4d1783cc0b93a71355dabac8c0cff5688e99e0
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-09-16 12:27:17 +0600

    First

---
+++ my-app/src/component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-xl font-bold'>
+                Hey!! Search Your Meal And feel with us
+            </h1>
+
+            <input type="search" className="p-2.5 w-2/4 text-center align-middle border-2 border-solid" placeholder="Search Your Food.." required></input>
+            <br></br>
+            <button className="m-4 p-3 border-black border-0 text-white rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500">Search</button>
+        </div >
+
+    );
+};
+
+export default Header;
